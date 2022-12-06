1589f3767f9e6610ca9a6388b81f48438dd80d2c
ANIRUDDH BALRAM <abalram1@umd.edu>
2022-12-06 08:19:42 -0500

    Changed the topic name to 'chatter'

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -67,7 +67,7 @@ class MinimalPublisher : public rclcpp::Node {
       get_parameter_value().get<std::float_t>();
     // Publisher initialization
     publisher_ = this->create_publisher<std_msgs::msg
-    ::String>("topic", 10);
+    ::String>("chatter", 10);
     // Set timer to 500ms(calls callback twice in a second)
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
